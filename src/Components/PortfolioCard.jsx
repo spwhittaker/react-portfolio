9950c273d5e09382d614aaa543d226e949f41bb4
Stephen Whittaker <stephenwhittaker23@gmail.com>
2020-03-13 12:10:48 +0000

    spacing on cards

--- src/Components/PortfolioCard.jsx
+++ src/Components/PortfolioCard.jsx
@@ -3,13 +3,11 @@ import "../Styles/PortfolioCard.css";
 const PortfolioCard = ({ link, portfolioImage, title, description }) => {
   return (
     <div className="portfolio-card">
-      <div className="flex-container">
-        <h3>
-          <a href={link}>{title}</a>
-        </h3>
-        <p>{description}</p>
-        <img src={portfolioImage} alt={title} className="portfolio-image" />
-      </div>
+      <h3>
+        <a href={link}>{title}</a>
+      </h3>
+      <p className="description">{description}</p>
+      <img src={portfolioImage} alt={title} className="portfolio-image" />
     </div>
   );
 };
